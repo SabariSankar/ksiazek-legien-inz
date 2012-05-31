509c31305cdedad82129809b4bd4b0518f0edbd4
haslo <haslo@autograf.pl>
2012-05-31 16:32:15 +0000

    zmiany wporwadzone przed spotkaniem u czekierdy

--- ActiVizPrototype/DicomLoadTest/Form1.cs
+++ ActiVizPrototype/DicomLoadTest/Form1.cs
@@ -26,18 +26,24 @@ namespace DicomLoadTest
         private void renderWindowControl1_Load(object sender, EventArgs e)
         {
             vtkRenderer renderer = renderWindowControl1.RenderWindow.GetRenderers().GetFirstRenderer();
-            vtkXMLImageDataReader reader = vtkXMLImageDataReader.New();
-            vtkFixedPointVolumeRayCastMapper texMapper = vtkFixedPointVolumeRayCastMapper.New();
+            
+            vtkSmartVolumeMapper mapper = vtkSmartVolumeMapper.New();
+            //vtkFixedPointVolumeRayCastMapper texMapper = vtkFixedPointVolumeRayCastMapper.New();
             vol = vtkVolume.New();
             ctf = vtkColorTransferFunction.New();
             spwf = vtkPiecewiseFunction.New();
             vtkPiecewiseFunction gpwf = vtkPiecewiseFunction.New();
 
-            reader.SetFileName("head.vti");
-            reader.Update();
+            //vtkXMLImageDataReader reader = vtkXMLImageDataReader.New();
+            //reader.SetFileName("head.vti");
+            //reader.Update();
 
             //Go through the visulizatin pipeline
-            texMapper.SetInputConnection(reader.GetOutputPort());
+            vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
+            dicomReader.SetDirectoryName("D:\\Downloads\\PANORAMIX");
+            dicomReader.Update();
+
+            mapper.SetInputConnection(dicomReader.GetOutputPort());
 
             //Set the color curve for the volume
             ctf.AddHSVPoint(1*x, .67, .07, 1);//1
@@ -60,7 +66,7 @@ namespace DicomLoadTest
             vol.GetProperty().SetScalarOpacity(spwf);
             vol.GetProperty().SetGradientOpacity(gpwf);
 
-            vol.SetMapper(texMapper);
+            vol.SetMapper(mapper);
 
             //Go through the Graphics Pipeline
             renderer.AddVolume(vol);
@@ -71,7 +77,8 @@ namespace DicomLoadTest
             trackBar1.Minimum = 0;
             trackBar1.Maximum = 150;
             x = trackBar1.Value;
-            //prowizoryczne zwiększanie współczymmika przenikalności
+
+
             spwf = vtkPiecewiseFunction.New();
             /*
             ctf.AddHSVPoint(0 + x, .67, .07, 1);//1
@@ -79,13 +86,19 @@ namespace DicomLoadTest
             ctf.AddHSVPoint(139 + x, 0, 0, 0);
             ctf.AddHSVPoint(160 + x, .28, .047, 1);
             ctf.AddHSVPoint(254 + x, .38, .013, 1);
+            */
+
+            ctf.AddRGBPoint(0 + x , 0, 0, 0);
+            ctf.AddRGBPoint(94 + x, .6, .7, 1);
+            ctf.AddRGBPoint(139 + x, 0, .9, .2);
+            ctf.AddRGBPoint(160 + x, .4, .47, .5);
+            ctf.AddRGBPoint(254 + x, .3, .13, .1);
 
             vol.GetProperty().SetColor(ctf);
-             * */
-            //
-            spwf.AddPoint(84+x, 0); //84
-            spwf.AddPoint(151, .1); //151
-            spwf.AddPoint(255, 1);
+ 
+            spwf.AddPoint(84 + x, 0); //84
+            spwf.AddPoint(151 + x, .1); //151
+            spwf.AddPoint(255 + x, 1);
             vol.GetProperty().SetScalarOpacity(spwf);
 
 
