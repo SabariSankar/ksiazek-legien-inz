4f745ae59072b43c3f6c025f2b889b86b84578dd
haslo@autograf.pl <haslo@autograf.pl@de43d424-067d-3954-792a-a5482421c2c8>
2012-06-12 16:55:55 +0000

    z suwakami

--- trunk/ActiVizPrototype/DicomLoadTest/Form1.Designer.cs
+++ trunk/ActiVizPrototype/DicomLoadTest/Form1.Designer.cs
@@ -28,128 +28,210 @@ namespace DicomLoadTest
         /// </summary>
         private void InitializeComponent()
         {
-            this.renderWindowControl1 = new Kitware.VTK.RenderWindowControl();
+            this.fourthWindow = new Kitware.VTK.RenderWindowControl();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.renderWindowControl4 = new Kitware.VTK.RenderWindowControl();
-            this.renderWindowControl2 = new Kitware.VTK.RenderWindowControl();
-            this.renderWindowControl3 = new Kitware.VTK.RenderWindowControl();
+            this.firstWindow = new Kitware.VTK.RenderWindowControl();
+            this.secondWindow = new Kitware.VTK.RenderWindowControl();
+            this.thirdWindow = new Kitware.VTK.RenderWindowControl();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLevel1 = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.labelThird = new System.Windows.Forms.Label();
+            this.labelSecond = new System.Windows.Forms.Label();
+            this.labelFirst = new System.Windows.Forms.Label();
+            this.labelLevel = new System.Windows.Forms.Label();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.trackBarLevel = new System.Windows.Forms.TrackBar();
+            this.trackBarThird = new System.Windows.Forms.TrackBar();
+            this.trackBarSecond = new System.Windows.Forms.TrackBar();
+            this.trackBarFirst = new System.Windows.Forms.TrackBar();
+            this.trackBarWidth = new System.Windows.Forms.TrackBar();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarLevel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWidth)).BeginInit();
             this.SuspendLayout();
             // 
-            // renderWindowControl1
+            // fourthWindow
             // 
-            this.renderWindowControl1.AddTestActors = false;
-            this.renderWindowControl1.Location = new System.Drawing.Point(360, 303);
-            this.renderWindowControl1.Name = "renderWindowControl1";
-            this.renderWindowControl1.Size = new System.Drawing.Size(351, 294);
-            this.renderWindowControl1.TabIndex = 0;
-            this.renderWindowControl1.TestText = null;
-            this.renderWindowControl1.Load += new System.EventHandler(this.renderWindowControl1_Load);
+            this.fourthWindow.AddTestActors = false;
+            this.fourthWindow.Location = new System.Drawing.Point(360, 303);
+            this.fourthWindow.Name = "fourthWindow";
+            this.fourthWindow.Size = new System.Drawing.Size(351, 294);
+            this.fourthWindow.TabIndex = 0;
+            this.fourthWindow.TestText = null;
+            this.fourthWindow.Load += new System.EventHandler(this.fourthWindow_Load);
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.renderWindowControl4);
-            this.flowLayoutPanel1.Controls.Add(this.renderWindowControl2);
-            this.flowLayoutPanel1.Controls.Add(this.renderWindowControl3);
-            this.flowLayoutPanel1.Controls.Add(this.renderWindowControl1);
+            this.flowLayoutPanel1.Controls.Add(this.firstWindow);
+            this.flowLayoutPanel1.Controls.Add(this.secondWindow);
+            this.flowLayoutPanel1.Controls.Add(this.thirdWindow);
+            this.flowLayoutPanel1.Controls.Add(this.fourthWindow);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(282, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(734, 611);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
-            // renderWindowControl4
+            // firstWindow
             // 
-            this.renderWindowControl4.AddTestActors = false;
-            this.renderWindowControl4.Location = new System.Drawing.Point(3, 3);
-            this.renderWindowControl4.Name = "renderWindowControl4";
-            this.renderWindowControl4.Size = new System.Drawing.Size(351, 294);
-            this.renderWindowControl4.TabIndex = 4;
-            this.renderWindowControl4.TestText = null;
-            this.renderWindowControl4.Load += new System.EventHandler(this.renderWindowControl4_Load);
+            this.firstWindow.AddTestActors = false;
+            this.firstWindow.Location = new System.Drawing.Point(3, 3);
+            this.firstWindow.Name = "firstWindow";
+            this.firstWindow.Size = new System.Drawing.Size(351, 294);
+            this.firstWindow.TabIndex = 4;
+            this.firstWindow.TestText = null;
+            this.firstWindow.Load += new System.EventHandler(this.firstWindow_Load);
             // 
-            // renderWindowControl2
+            // secondWindow
             // 
-            this.renderWindowControl2.AddTestActors = false;
-            this.renderWindowControl2.Location = new System.Drawing.Point(360, 3);
-            this.renderWindowControl2.Name = "renderWindowControl2";
-            this.renderWindowControl2.Size = new System.Drawing.Size(351, 294);
-            this.renderWindowControl2.TabIndex = 3;
-            this.renderWindowControl2.TestText = null;
+            this.secondWindow.AddTestActors = false;
+            this.secondWindow.Location = new System.Drawing.Point(360, 3);
+            this.secondWindow.Name = "secondWindow";
+            this.secondWindow.Size = new System.Drawing.Size(351, 294);
+            this.secondWindow.TabIndex = 3;
+            this.secondWindow.TestText = null;
+            this.secondWindow.Load += new System.EventHandler(this.secondWindow_Load);
             // 
-            // renderWindowControl3
+            // thirdWindow
             // 
-            this.renderWindowControl3.AddTestActors = false;
-            this.renderWindowControl3.Location = new System.Drawing.Point(3, 303);
-            this.renderWindowControl3.Name = "renderWindowControl3";
-            this.renderWindowControl3.Size = new System.Drawing.Size(351, 294);
-            this.renderWindowControl3.TabIndex = 4;
-            this.renderWindowControl3.TestText = null;
+            this.thirdWindow.AddTestActors = false;
+            this.thirdWindow.Location = new System.Drawing.Point(3, 303);
+            this.thirdWindow.Name = "thirdWindow";
+            this.thirdWindow.Size = new System.Drawing.Size(351, 294);
+            this.thirdWindow.TabIndex = 4;
+            this.thirdWindow.TestText = null;
+            this.thirdWindow.Load += new System.EventHandler(this.thirdWindow_Load);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.comboBox2);
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxLevel1);
+            this.panel1.Controls.Add(this.textBoxWidth);
+            this.panel1.Controls.Add(this.labelThird);
+            this.panel1.Controls.Add(this.labelSecond);
+            this.panel1.Controls.Add(this.labelFirst);
+            this.panel1.Controls.Add(this.labelLevel);
+            this.panel1.Controls.Add(this.labelWidth);
+            this.panel1.Controls.Add(this.trackBarLevel);
+            this.panel1.Controls.Add(this.trackBarThird);
+            this.panel1.Controls.Add(this.trackBarSecond);
+            this.panel1.Controls.Add(this.trackBarFirst);
+            this.panel1.Controls.Add(this.trackBarWidth);
             this.panel1.Location = new System.Drawing.Point(12, 7);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(254, 608);
             this.panel1.TabIndex = 3;
             // 
-            // comboBox2
+            // textBoxLevel
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "-700",
-            "350",
-            "40",
-            "1000",
-            "10"});
-            this.comboBox2.Location = new System.Drawing.Point(108, 130);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 5;
-            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.textBoxLevel1.Location = new System.Drawing.Point(80, 293);
+            this.textBoxLevel1.Name = "textBoxLevel";
+            this.textBoxLevel1.Size = new System.Drawing.Size(57, 20);
+            this.textBoxLevel1.TabIndex = 17;
+            this.textBoxLevel1.Text = "0";
+            this.trackBarLevel.Value = 0;
+            this.textBoxLevel1.TextChanged += new System.EventHandler(this.textBoxLevel_TextChanged);
             // 
-            // comboBox1
+            // textBoxWidth
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "1000",
-            "2000",
-            "400",
-            "30",
-            "10",
-            "5"});
-            this.comboBox1.Location = new System.Drawing.Point(108, 80);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 4;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.textBoxWidth.Location = new System.Drawing.Point(80, 215);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(57, 20);
+            this.textBoxWidth.TabIndex = 16;
+            this.textBoxWidth.Text = "0";
+            this.trackBarWidth.Value = 0;
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBoxWidth_TextChanged);
             // 
-            // label2
+            // labelThird
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 138);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Window level:";
+            this.labelThird.AutoSize = true;
+            this.labelThird.Location = new System.Drawing.Point(41, 558);
+            this.labelThird.Name = "labelThird";
+            this.labelThird.Size = new System.Drawing.Size(13, 13);
+            this.labelThird.TabIndex = 15;
+            this.labelThird.Text = "3";
             // 
-            // label1
+            // labelSecond
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 83);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Window width: ";
+            this.labelSecond.AutoSize = true;
+            this.labelSecond.Location = new System.Drawing.Point(41, 501);
+            this.labelSecond.Name = "labelSecond";
+            this.labelSecond.Size = new System.Drawing.Size(13, 13);
+            this.labelSecond.TabIndex = 14;
+            this.labelSecond.Text = "2";
+            // 
+            // labelFirst
+            // 
+            this.labelFirst.AutoSize = true;
+            this.labelFirst.Location = new System.Drawing.Point(41, 447);
+            this.labelFirst.Name = "labelFirst";
+            this.labelFirst.Size = new System.Drawing.Size(13, 13);
+            this.labelFirst.TabIndex = 13;
+            this.labelFirst.Text = "1";
+            // 
+            // labelLevel
+            // 
+            this.labelLevel.AutoSize = true;
+            this.labelLevel.Location = new System.Drawing.Point(41, 300);
+            this.labelLevel.Name = "labelLevel";
+            this.labelLevel.Size = new System.Drawing.Size(33, 13);
+            this.labelLevel.TabIndex = 12;
+            this.labelLevel.Text = "Level";
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(39, 220);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(35, 13);
+            this.labelWidth.TabIndex = 11;
+            this.labelWidth.Text = "Width";
+            // 
+            // trackBarLevel
+            // 
+            this.trackBarLevel.Location = new System.Drawing.Point(26, 325);
+            this.trackBarLevel.Maximum = 1500;
+            this.trackBarLevel.Minimum = -700;
+            this.trackBarLevel.Name = "trackBarLevel";
+            this.trackBarLevel.Size = new System.Drawing.Size(203, 45);
+            this.trackBarLevel.TabIndex = 10;
+            this.trackBarLevel.Scroll += new System.EventHandler(this.trackBarLevel_Scroll);
+            // 
+            // trackBarThird
+            // 
+            this.trackBarThird.Location = new System.Drawing.Point(108, 549);
+            this.trackBarThird.Name = "trackBarThird";
+            this.trackBarThird.Size = new System.Drawing.Size(104, 45);
+            this.trackBarThird.TabIndex = 9;
+            this.trackBarThird.Scroll += new System.EventHandler(this.trackBarThird_Scroll);
+            // 
+            // trackBarSecond
+            // 
+            this.trackBarSecond.Location = new System.Drawing.Point(108, 498);
+            this.trackBarSecond.Name = "trackBarSecond";
+            this.trackBarSecond.Size = new System.Drawing.Size(104, 45);
+            this.trackBarSecond.TabIndex = 8;
+            this.trackBarSecond.Scroll += new System.EventHandler(this.trackBarSecond_Scroll);
+            // 
+            // trackBarFirst
+            // 
+            this.trackBarFirst.Location = new System.Drawing.Point(108, 447);
+            this.trackBarFirst.Name = "trackBarFirst";
+            this.trackBarFirst.Size = new System.Drawing.Size(104, 45);
+            this.trackBarFirst.TabIndex = 7;
+            this.trackBarFirst.Scroll += new System.EventHandler(this.trackBarFirst_Scroll);
+            // 
+            // trackBarWidth
+            // 
+            this.trackBarWidth.Location = new System.Drawing.Point(26, 249);
+            this.trackBarWidth.Maximum = 1000;
+            this.trackBarWidth.Name = "trackBarWidth";
+            this.trackBarWidth.Size = new System.Drawing.Size(203, 45);
+            this.trackBarWidth.TabIndex = 6;
+            this.trackBarWidth.Scroll += new System.EventHandler(this.trackBarWidth_Scroll);
             // 
             // Form1
             // 
@@ -163,22 +245,35 @@ namespace DicomLoadTest
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarLevel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWidth)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Kitware.VTK.RenderWindowControl renderWindowControl1;
+        private Kitware.VTK.RenderWindowControl fourthWindow;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private Kitware.VTK.RenderWindowControl renderWindowControl2;
-        private Kitware.VTK.RenderWindowControl renderWindowControl3;
-        private Kitware.VTK.RenderWindowControl renderWindowControl4;
+        private Kitware.VTK.RenderWindowControl secondWindow;
+        private Kitware.VTK.RenderWindowControl thirdWindow;
+        private Kitware.VTK.RenderWindowControl firstWindow;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelLevel;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.TrackBar trackBarLevel;
+        private System.Windows.Forms.TrackBar trackBarThird;
+        private System.Windows.Forms.TrackBar trackBarSecond;
+        private System.Windows.Forms.TrackBar trackBarFirst;
+        private System.Windows.Forms.TrackBar trackBarWidth;
+        private System.Windows.Forms.Label labelThird;
+        private System.Windows.Forms.Label labelSecond;
+        private System.Windows.Forms.Label labelFirst;
+        private System.Windows.Forms.TextBox textBoxLevel1;
+        private System.Windows.Forms.TextBox textBoxWidth;
     }
 }
 
--- trunk/ActiVizPrototype/DicomLoadTest/Form1.cs
+++ trunk/ActiVizPrototype/DicomLoadTest/Form1.cs
@@ -18,31 +18,33 @@ namespace DicomLoadTest
             InitializeComponent();
         }
 
-        public static float x = 1;
+        String directoryName = "D:\\Downloads\\PANORAMIX\\";
+
         vtkColorTransferFunction ctf;
         vtkPiecewiseFunction spwf;
         vtkVolume vol;
         float windowWidth = 0;
         float windowLevel = 40;
 
-        private void renderWindowControl1_Load(object sender, EventArgs e)
+        vtkImageViewer2 viewer1;    // viewer for first renderWindowControl - x
+        vtkImageViewer2 viewer2;    // viewer for second renderWindowControl - y
+        vtkImageViewer2 viewer3;    // viewer for third renderWindowControl - z
+
+
+        //wizualizacja 3d -----------------------------------------------------------------
+        private void fourthWindow_Load(object sender, EventArgs e)
         {
-            vtkRenderer renderer = renderWindowControl1.RenderWindow.GetRenderers().GetFirstRenderer();
+            vtkRenderer renderer = fourthWindow.RenderWindow.GetRenderers().GetFirstRenderer();
             
             vtkSmartVolumeMapper mapper = vtkSmartVolumeMapper.New();
-            //vtkFixedPointVolumeRayCastMapper texMapper = vtkFixedPointVolumeRayCastMapper.New();
             vol = vtkVolume.New();
             ctf = vtkColorTransferFunction.New();
             spwf = vtkPiecewiseFunction.New();
             vtkPiecewiseFunction gpwf = vtkPiecewiseFunction.New();
 
-            //vtkXMLImageDataReader reader = vtkXMLImageDataReader.New();
-            //reader.SetFileName("head.vti");
-            //reader.Update();
-
             //Go through the visulizatin pipeline
             vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
-            dicomReader.SetDirectoryName("D:\\Downloads\\PANORAMIX");
+            dicomReader.SetDirectoryName(directoryName);
             dicomReader.Update();
 
             mapper.SetInputConnection(dicomReader.GetOutputPort());
@@ -74,66 +76,161 @@ namespace DicomLoadTest
             vol.GetProperty().SetScalarOpacity(spwf);
             vol.GetProperty().SetGradientOpacity(gpwf);
 
+                            //http://informatyka.umcs.lublin.pl/files/siczek.pdf
+
             vol.SetMapper(mapper);
 
             //Go through the Graphics Pipeline
             renderer.AddVolume(vol);
         }
 
+        //updatatuje okno wziualizacji 3d
+        private void update3DVisualization()
+        {
 
-        private void renderWindowControl4_Load(object sender, EventArgs e)
+            spwf = vtkPiecewiseFunction.New();
+            spwf.AddPoint(this.windowLevel - (this.windowWidth / 2), 0);
+            spwf.AddPoint(this.windowLevel, 1);
+            spwf.AddPoint(this.windowLevel + (this.windowWidth / 2), 0);
+            vol.GetProperty().SetScalarOpacity(spwf);
+
+            this.fourthWindow.Validate();
+            this.fourthWindow.Update();
+        }
+
+        //suwak obsluguje szerokosc ------------------------------------------------------------
+        private void trackBarWidth_Scroll(object sender, EventArgs e)
+        {
+
+            this.windowWidth = float.Parse(trackBarWidth.Value.ToString());
+            this.update3DVisualization();
+            this.textBoxWidth.Text = trackBarWidth.Value.ToString();
+
+        }
+
+        //suwak obsluguje level ------------------------------------------------------------
+        private void trackBarLevel_Scroll(object sender, EventArgs e)
+        {
+            this.windowLevel = float.Parse(trackBarLevel.Value.ToString());
+            this.update3DVisualization();
+            this.textBoxLevel1.Text = trackBarLevel.Value.ToString();
+        }
+
+
+        //wizualizaja 2d
+        private void firstWindow_Load(object sender, EventArgs e)
         {
 
             vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
-            dicomReader.SetDirectoryName("D:\\Downloads\\PANORAMIX\\");
+            dicomReader.SetDirectoryName(directoryName);
             dicomReader.Update();
 
             //create image viewer
+            viewer1 = vtkImageViewer2.New();
+            viewer1.SetInputConnection(dicomReader.GetOutputPort());
 
-            vtkImageViewer2 viewer = vtkImageViewer2.New();
-            viewer.SetInputConnection(dicomReader.GetOutputPort());
+            viewer1.OffScreenRenderingOn();
+            int max = viewer1.GetSliceMax();
+            viewer1.SetSlice(100);
+            trackBarFirst.SetRange(0, max);
 
-        
-            viewer.OffScreenRenderingOn(); //dont show window of load images
-            int VolData_Images = viewer.GetSliceMax();
-            viewer.SetSlice(100); //slice showed
-
-            viewer.Render();
-            renderWindowControl4.RenderWindow.AddRenderer(viewer.GetRenderer());
+            viewer1.Render();
+            firstWindow.RenderWindow.AddRenderer(viewer1.GetRenderer());
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void secondWindow_Load(object sender, EventArgs e)
         {
-            this.windowWidth = float.Parse(comboBox1.SelectedItem.ToString());
+            vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
+            dicomReader.SetDirectoryName(directoryName);
+            dicomReader.Update();
 
-            spwf = vtkPiecewiseFunction.New();
-            spwf.AddPoint(this.windowLevel - (this.windowWidth / 2), 0);
-            spwf.AddPoint(this.windowLevel, 1);
-            spwf.AddPoint(this.windowLevel + (this.windowWidth / 2), 0);
-            vol.GetProperty().SetScalarOpacity(spwf);
+            //create image viewer
+            viewer2 = vtkImageViewer2.New();
+            viewer2.SetInputConnection(dicomReader.GetOutputPort());
 
-            this.renderWindowControl1.Validate();
-            this.renderWindowControl1.Update();
+
+            viewer2.OffScreenRenderingOn();
+            int max = viewer2.GetSliceMax();
+            viewer2.SetSlice(50);
+            trackBarSecond.SetRange(0, max);
+
+            viewer2.Render();
+            secondWindow.RenderWindow.AddRenderer(viewer2.GetRenderer());
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void thirdWindow_Load(object sender, EventArgs e)
         {
-            this.windowLevel = float.Parse(comboBox2.SelectedItem.ToString());
+            vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
+            dicomReader.SetDirectoryName(directoryName);
+            dicomReader.Update();
 
-            spwf = vtkPiecewiseFunction.New();
-            spwf.AddPoint(this.windowLevel - (this.windowWidth / 2), 0);
-            spwf.AddPoint(this.windowLevel, 1);
-            spwf.AddPoint(this.windowLevel + (this.windowWidth / 2), 0);
-            vol.GetProperty().SetScalarOpacity(spwf);
+            //create image viewer
+            viewer3 = vtkImageViewer2.New();
+            viewer3.SetInputConnection(dicomReader.GetOutputPort());
 
-            this.renderWindowControl1.Validate();
-            this.renderWindowControl1.Update();
+
+            viewer3.OffScreenRenderingOn();
+            int max = viewer3.GetSliceMax();
+            viewer3.SetSlice(150);
+            trackBarThird.SetRange(0, max);
+
+            viewer3.Render();
+            thirdWindow.RenderWindow.AddRenderer(viewer3.GetRenderer());
         }
 
-  
+
+        //suwaki obslugujace 2d  -----------------------------------------------------
+        private void trackBarFirst_Scroll(object sender, EventArgs e)
+        {
+            int slice = int.Parse(trackBarFirst.Value.ToString());
+            viewer1.SetSlice(slice);
+            viewer1.Render();
+
+            this.firstWindow.Validate();
+            this.firstWindow.Update();
+        }
+
+        private void trackBarSecond_Scroll(object sender, EventArgs e)
+        {
+            int slice = int.Parse(trackBarSecond.Value.ToString());
+            viewer2.SetSlice(slice);
+            viewer2.Render();
+
+            this.secondWindow.Validate();
+            this.secondWindow.Update();
+        }
+
+        private void trackBarThird_Scroll(object sender, EventArgs e)
+        {
+            int slice = int.Parse(trackBarThird.Value.ToString());
+            viewer3.SetSlice(slice);
+            viewer3.Render();
+
+            this.thirdWindow.Validate();
+            this.thirdWindow.Update();
+        }
+
+        private void textBoxWidth_TextChanged(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBoxWidth.Text.ToString());
+            this.trackBarWidth.Value = x;
+            this.windowWidth = float.Parse(textBoxWidth.Text.ToString());
+
+            this.update3DVisualization();
+        }
+
+        private void textBoxLevel_TextChanged(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBoxLevel1.Text.ToString());
+            this.trackBarLevel.Value = x;
+            this.windowLevel = float.Parse(textBoxLevel1.Text.ToString());
+
+            this.update3DVisualization();
+        }
 
 
-   
     }
 }
