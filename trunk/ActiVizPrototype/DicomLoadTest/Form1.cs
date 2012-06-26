65e56c2543b7d3afd9d4391e2f5892ed549857b8
haslo@autograf.pl <haslo@autograf.pl@de43d424-067d-3954-792a-a5482421c2c8>
2012-06-26 07:51:15 +0000

    przykladowy preset

--- trunk/ActiVizPrototype/DicomLoadTest/Form1.cs
+++ trunk/ActiVizPrototype/DicomLoadTest/Form1.cs
@@ -13,89 +13,51 @@ namespace DicomLoadTest
 {
     public partial class Form1 : Form
     {
+        Visualization3D vizualization3D;
+        Visualization2D firstVizualization2D;
+        Visualization2D secondVizualization2D;
+        Visualization2D thirdVizualization2D;
+
+        String directoryName = "D:\\Downloads\\PANORAMIX\\";
+
+        float windowWidth = 0;
+        float windowLevel = 40;
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        String directoryName = "D:\\Downloads\\PANORAMIX\\";
-
-        vtkColorTransferFunction ctf;
-        vtkPiecewiseFunction spwf;
-        vtkVolume vol;
-        float windowWidth = 0;
-        float windowLevel = 40;
-
-        vtkImageViewer2 viewer1;    // viewer for first renderWindowControl - x
-        vtkImageViewer2 viewer2;    // viewer for second renderWindowControl - y
-        vtkImageViewer2 viewer3;    // viewer for third renderWindowControl - z
-
-
         //wizualizacja 3d -----------------------------------------------------------------
         private void fourthWindow_Load(object sender, EventArgs e)
         {
-            vtkRenderer renderer = fourthWindow.RenderWindow.GetRenderers().GetFirstRenderer();
-            
-            vtkSmartVolumeMapper mapper = vtkSmartVolumeMapper.New();
-            vol = vtkVolume.New();
-            ctf = vtkColorTransferFunction.New();
-            spwf = vtkPiecewiseFunction.New();
-            vtkPiecewiseFunction gpwf = vtkPiecewiseFunction.New();
-
-            //Go through the visulizatin pipeline
             vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
             dicomReader.SetDirectoryName(directoryName);
             dicomReader.Update();
 
-            mapper.SetInputConnection(dicomReader.GetOutputPort());
+            vizualization3D = new Visualization3D(fourthWindow,dicomReader);
 
-            //Set the color curve for the volume
-            ctf.AddRGBPoint(-100, .9, .9, .5);       //tluszcz 50 - -100
-            ctf.AddRGBPoint(0, .6, .45, .5);          //woda ~0
-            ctf.AddRGBPoint(40, 1, 0, 0);             //krew ~40
-            ctf.AddRGBPoint(50, 1, .1, .1);           //watroba 40-60
-            ctf.AddRGBPoint(37, .4, .4, .3);         //istota szara mozgu 37-45
-            //istota biala mozgu 20 - 30
-            //miazsz nerki ~30
-            //plyn mozgowo-rdzenowy 10-15
-            ctf.AddRGBPoint(20, 1, 0, 0);         //miesnie 10 - 40
-            ctf.AddRGBPoint(1500, 1, 1,1);           //kosc 1000 - 1500
-
-            //Set the opacity curve for the volume
-            spwf.AddPoint(this.windowLevel - (this.windowWidth / 2), 0);
-            spwf.AddPoint(this.windowLevel, 1);
-            spwf.AddPoint(this.windowLevel + (this.windowWidth / 2), 0);
-
-
-            //Set the gradient curve for the volume
-            gpwf.AddPoint(0, .2);
-            gpwf.AddPoint(10, .2);
-            gpwf.AddPoint(25, 1);
-
-            vol.GetProperty().SetColor(ctf);
-            vol.GetProperty().SetScalarOpacity(spwf);
-            vol.GetProperty().SetGradientOpacity(gpwf);
-
-                            //http://informatyka.umcs.lublin.pl/files/siczek.pdf
-
-            vol.SetMapper(mapper);
-
-            //Go through the Graphics Pipeline
-            renderer.AddVolume(vol);
+            PresetMapper presets = new PresetMapper();
+            foreach (string nameOfPreset in presets.Presets)
+            {
+                comboBox1.Items.Add(nameOfPreset);
+            }
+            comboBox1.SelectedText = presets.Presets[0];
         }
 
         //updatatuje okno wziualizacji 3d
         private void update3DVisualization()
         {
+            vizualization3D.update3DVisualization(this.windowLevel,this.windowWidth);
+        }
 
-            spwf = vtkPiecewiseFunction.New();
-            spwf.AddPoint(this.windowLevel - (this.windowWidth / 2), 0);
-            spwf.AddPoint(this.windowLevel, 1);
-            spwf.AddPoint(this.windowLevel + (this.windowWidth / 2), 0);
-            vol.GetProperty().SetScalarOpacity(spwf);
 
-            this.fourthWindow.Validate();
-            this.fourthWindow.Update();
+        private void update2DVisualization()
+        {
+            this.firstVizualization2D.update2DVisualization(this.windowLevel,this.windowWidth);
+            this.secondVizualization2D.update2DVisualization(this.windowLevel,this.windowWidth);
+            this.thirdVizualization2D.update2DVisualization(this.windowLevel,this.windowWidth);
         }
 
         //suwak obsluguje szerokosc ------------------------------------------------------------
@@ -104,6 +66,7 @@ namespace DicomLoadTest
 
             this.windowWidth = float.Parse(trackBarWidth.Value.ToString());
             this.update3DVisualization();
+            this.update2DVisualization();
             this.textBoxWidth.Text = trackBarWidth.Value.ToString();
 
         }
@@ -113,6 +76,7 @@ namespace DicomLoadTest
         {
             this.windowLevel = float.Parse(trackBarLevel.Value.ToString());
             this.update3DVisualization();
+            this.update2DVisualization();
             this.textBoxLevel1.Text = trackBarLevel.Value.ToString();
         }
 
@@ -120,115 +84,47 @@ namespace DicomLoadTest
         //wizualizaja 2d
         private void firstWindow_Load(object sender, EventArgs e)
         {
-
-            vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
-            dicomReader.SetDirectoryName(directoryName);
-            dicomReader.Update();
-
-            //create image viewer
-            viewer1 = vtkImageViewer2.New();
-            viewer1.SetInputConnection(dicomReader.GetOutputPort());
-
-            viewer1.OffScreenRenderingOn();
-            int max = viewer1.GetSliceMax();
-            viewer1.SetSlice(100);
-            trackBarFirst.SetRange(0, max);
-
-            viewer1.Render();
-            firstWindow.RenderWindow.AddRenderer(viewer1.GetRenderer());
-
+            firstVizualization2D = new Visualization2D(firstWindow,directoryName);
         }
 
 
         private void secondWindow_Load(object sender, EventArgs e)
         {
-            vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
-            dicomReader.SetDirectoryName(directoryName);
-            dicomReader.Update();
-
-            //create image viewer
-            viewer2 = vtkImageViewer2.New();
-            viewer2.SetInputConnection(dicomReader.GetOutputPort());
-
-
-            viewer2.OffScreenRenderingOn();
-            int max = viewer2.GetSliceMax();
-            viewer2.SetSlice(50);
-            trackBarSecond.SetRange(0, max);
-
-            viewer2.Render();
-            secondWindow.RenderWindow.AddRenderer(viewer2.GetRenderer());
+            secondVizualization2D = new Visualization2D(firstWindow, directoryName);
         }
 
 
         private void thirdWindow_Load(object sender, EventArgs e)
         {
-            vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
-            dicomReader.SetDirectoryName(directoryName);
-            dicomReader.Update();
-
-            //create image viewer
-            viewer3 = vtkImageViewer2.New();
-            viewer3.SetInputConnection(dicomReader.GetOutputPort());
-
-
-            viewer3.OffScreenRenderingOn();
-            int max = viewer3.GetSliceMax();
-            viewer3.SetSlice(150);
-            trackBarThird.SetRange(0, max);
-
-            viewer3.Render();
-            thirdWindow.RenderWindow.AddRenderer(viewer3.GetRenderer());
+            thirdVizualization2D = new Visualization2D(secondWindow, directoryName);
         }
 
-
-        //suwaki obslugujace 2d  -----------------------------------------------------
-        private void trackBarFirst_Scroll(object sender, EventArgs e)
-        {
-            int slice = int.Parse(trackBarFirst.Value.ToString());
-            viewer1.SetSlice(slice);
-            viewer1.Render();
-
-            this.firstWindow.Validate();
-            this.firstWindow.Update();
-        }
-
-        private void trackBarSecond_Scroll(object sender, EventArgs e)
-        {
-            int slice = int.Parse(trackBarSecond.Value.ToString());
-            viewer2.SetSlice(slice);
-            viewer2.Render();
-
-            this.secondWindow.Validate();
-            this.secondWindow.Update();
-        }
-
-        private void trackBarThird_Scroll(object sender, EventArgs e)
-        {
-            int slice = int.Parse(trackBarThird.Value.ToString());
-            viewer3.SetSlice(slice);
-            viewer3.Render();
-
-            this.thirdWindow.Validate();
-            this.thirdWindow.Update();
-        }
-
+        //zmiany textboxow
         private void textBoxWidth_TextChanged(object sender, EventArgs e)
         {
-            int x = int.Parse(textBoxWidth.Text.ToString());
+            int x = int.Parse(textBoxWidth.Text);
             this.trackBarWidth.Value = x;
-            this.windowWidth = float.Parse(textBoxWidth.Text.ToString());
+            this.windowWidth = float.Parse(textBoxWidth.Text);
 
             this.update3DVisualization();
         }
 
         private void textBoxLevel_TextChanged(object sender, EventArgs e)
         {
-            int x = int.Parse(textBoxLevel1.Text.ToString());
+            int x = int.Parse(textBoxLevel1.Text);
             this.trackBarLevel.Value = x;
-            this.windowLevel = float.Parse(textBoxLevel1.Text.ToString());
+            this.windowLevel = float.Parse(textBoxLevel1.Text);
 
             this.update3DVisualization();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            vizualization3D.changeColorFunction(comboBox1.Text);
+            vizualization3D.changeOpacityFunction(comboBox1.Text);
+
+            this.update3DVisualization();
+
         }
 
 
---
+++ trunk/ActiVizPrototype/DicomLoadTest/Visualization2D.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kitware.VTK;
+
+namespace DicomLoadTest
+{
+    public class Visualization2D
+    {
+        private RenderWindowControl window;
+        private vtkColorTransferFunction ctf;
+        private vtkPiecewiseFunction spwf;
+        private vtkVolume vol;
+
+        private float windowWidth = 0;
+        private float windowLevel = 40;
+
+
+        public Visualization2D(RenderWindowControl window, String directoryName)
+        {
+            this.window = window;
+         
+            vtkDICOMImageReader dicomReader = vtkDICOMImageReader.New();
+            dicomReader.SetFileName(directoryName + "IM-0001-0050.dcm");
+            dicomReader.Update();
+
+            vtkRenderer renderer = window.RenderWindow.GetRenderers().GetFirstRenderer();
+
+            vtkSmartVolumeMapper mapper = vtkSmartVolumeMapper.New();
+            vol = vtkVolume.New();
+            spwf = vtkPiecewiseFunction.New();
+            vtkPiecewiseFunction gpwf = vtkPiecewiseFunction.New();
+
+            mapper.SetInputConnection(dicomReader.GetOutputPort());
+
+            //Set the opacity curve for the volume
+            spwf.AddPoint(this.windowLevel - (this.windowWidth / 2), 1);
+            spwf.AddPoint(this.windowLevel, 0);
+            spwf.AddPoint(this.windowLevel + (this.windowWidth / 2), 1);
+
+
+            //Set the gradient curve for the volume
+            gpwf.AddPoint(0, .2);
+            gpwf.AddPoint(10, .2);
+            gpwf.AddPoint(25, 1);
+
+            vol.GetProperty().SetScalarOpacity(spwf);
+            vol.GetProperty().SetGradientOpacity(gpwf);
+
+            vol.SetMapper(mapper);
+
+            //Go through the Graphics Pipeline
+            renderer.AddVolume(vol);
+        }
+
+        public void update2DVisualization(float windowLevel, float windowWidth)
+        {
+            this.windowLevel = windowLevel;
+            this.windowWidth = windowWidth;
+
+            spwf = vtkPiecewiseFunction.New();
+            spwf.AddPoint(this.windowLevel - (this.windowWidth / 2), 1);
+            spwf.AddPoint(this.windowLevel, 0);
+            spwf.AddPoint(this.windowLevel + (this.windowWidth / 2), 1);
+            vol.GetProperty().SetScalarOpacity(spwf);
+
+            this.window.Validate();
+            this.window.Update();
+            this.window.RenderWindow.Render();
+
+        }
+
+    }
+}
