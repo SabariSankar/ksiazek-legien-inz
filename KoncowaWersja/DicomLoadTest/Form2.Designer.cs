6b702719eb3a8478398fc30666f3147d9251e127
haslo <haslo@autograf.pl>
2012-12-10 12:33:05 +0000

    Wersja którą oddawaliśmy ostatnio Czekierdzie

---
+++ KoncowaWersja/DicomLoadTest/Form2.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace MainWindow
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.renderWindowControl = new Kitware.VTK.RenderWindowControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1308, 760);
+            this.tabControl.TabIndex = 0;
+            this.tabControl.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.trackBar);
+            this.tabPage1.Controls.Add(this.renderWindowControl);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1300, 734);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(1251, 276);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar.Size = new System.Drawing.Size(45, 104);
+            this.trackBar.TabIndex = 1;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // renderWindowControl
+            // 
+            this.renderWindowControl.AddTestActors = false;
+            this.renderWindowControl.Location = new System.Drawing.Point(4, 7);
+            this.renderWindowControl.Name = "renderWindowControl";
+            this.renderWindowControl.Size = new System.Drawing.Size(1081, 584);
+            this.renderWindowControl.TabIndex = 0;
+            this.renderWindowControl.TestText = null;
+            this.renderWindowControl.Load += new System.EventHandler(this.renderWindowControl1_Load);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1300, 734);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1293, 730);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1308, 759);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private Kitware.VTK.RenderWindowControl renderWindowControl;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar trackBar;
+
+    }
+}
---
+++ KoncowaWersja/DicomLoadTest/Visualization2D.cs
@@ -0,0 +1,198 @@
+﻿using Kitware.VTK;
+
+namespace MainWindow
+{
+
+    public enum RotationOperation
+    {
+        Back,
+        Forward
+    };
+
+    /// <summary>
+    /// Takes care of 2D visualization window.
+    /// </summary>
+    public class Visualization2D
+    {
+        /// <summary>
+        /// Render window of current visualization 2D.
+        /// </summary>
+        private readonly RenderWindowControl _window;
+        /// <summary>
+        /// Viewer of current visualization 2D. Takes care of proper display slice inside the window.
+        /// </summary>
+        private readonly vtkImageViewer2 _viewer;
+        /// <summary>
+        /// Window width of the visualization 2D.
+        /// </summary>
+        private float _windowWidth = 100;
+        /// <summary>
+        /// Window level of the visualization 2D.
+        /// </summary>
+        private float _windowLevel = 100;
+        /// <summary>
+        /// Orientation of the current visualization 2D (in degrees).
+        /// </summary>
+        private int _orientation;
+
+
+        public void RotateImageForward()
+        {
+            _orientation = _orientation + 90;
+            if (_orientation == 360) _orientation = 0;
+            RotateImage(RotationOperation.Forward);
+        }
+
+        public void RotateImageBack()
+        {
+            _orientation = _orientation - 90;
+            if (_orientation == -90) _orientation = 270;
+            RotateImage(RotationOperation.Back);
+        }
+
+        private void RotateImage(RotationOperation operation)
+        {
+            vtkImageReslice reslice = vtkImageReslice.New();
+            vtkTransform transform = vtkTransform.New();
+            transform.PostMultiply();
+
+            double[] center = { 75, 100, 0 };
+            transform.Translate(-center[0], -center[1], -center[2]);
+            if (operation == RotationOperation.Forward)
+            {
+                transform.RotateZ(90);
+            }
+            else if (operation == RotationOperation.Back)
+            {
+                transform.RotateZ(-90);
+            }
+            transform.Translate(+center[0], +center[1], +center[2]);
+
+            transform.Update();
+            reslice.SetInput(_viewer.GetInput());
+            reslice.SetResliceTransform(transform);
+            reslice.Update();
+
+            _viewer.SetInput(reslice.GetOutput());
+            UpdateViewer();
+        }
+
+
+        /// <summary>
+        /// Update the 2D visualization when the plane moved.
+        /// </summary>
+        /// <param name="plane">PlaneWidget which changed the coordinates.</param>
+        public void PlaneMoved(vtkImagePlaneWidget plane)
+        {
+
+            vtkImageReslice reslice = vtkImageReslice.New();
+            vtkTransform transform = vtkTransform.New();
+            transform.PostMultiply();
+
+            //TODO wyznaczenie centrum okna
+            double[] center = {75, 100, 0};
+			transform.Translate( -center[0], -center[1], -center[2] );
+            transform.RotateZ(_orientation);
+			transform.Translate( +center[0], +center[1], +center[2] );
+
+            transform.Update();
+            reslice.SetInput(plane.GetResliceOutput());
+            reslice.SetResliceTransform(transform);
+            reslice.Update();
+
+            _viewer.SetInput(reslice.GetOutput());
+            UpdateViewer();
+
+        }
+
+
+        /// <summary>
+        /// Update the 2D visualization window with new slice of pass X, Y or Z coordination. 
+        /// </summary>
+        /// <param name="dicomLoader"> Dicom input from we are going to cut the slice.</param>
+        /// <param name="slicePosition">Coordinates of the slice. </param>
+        /// <param name="axis">Name of axis to set cut orientation (X,Y,Z). </param>
+        public void SliceToAxes(DicomLoader dicomLoader, float slicePosition, Axis axis)
+        {
+            vtkImagePlaneWidget planeWidget = vtkImagePlaneWidget.New();
+            planeWidget.SetInput(dicomLoader.GetOutput());
+            if (axis == Axis.X)
+            {
+                planeWidget.SetPlaneOrientationToXAxes();
+            }
+            else if (axis == Axis.Y)
+            {
+                planeWidget.SetPlaneOrientationToYAxes();
+            }
+            else if (axis == Axis.Z)
+            {
+                planeWidget.SetPlaneOrientationToZAxes();
+            }
+            planeWidget.SetSliceIndex((int)slicePosition);
+            _viewer.SetInput(planeWidget.GetResliceOutput());
+            planeWidget.Dispose();
+
+            UpdateViewer();
+        }
+
+        /// <summary>
+        /// Refresh viewer and window.
+        /// </summary>
+        private void UpdateViewer()
+        {
+            _viewer.SetColorWindow(_windowWidth);
+            _viewer.SetColorLevel(_windowLevel);
+            _viewer.Render();
+            _window.Update();
+            _window.RenderWindow.Render();
+        }
+
+        /// <summary>
+        /// Creating the 2D visualization window.
+        /// </summary>
+        /// <param name="window">Orginal window component. </param>
+        public Visualization2D(RenderWindowControl window)
+        {
+            _window = window;
+            vtkInteractorStyleImage imageStyle = vtkInteractorStyleImage.New();
+
+            _viewer = vtkImageViewer2.New();
+            _viewer.OffScreenRenderingOn();
+            _window.RenderWindow.AddRenderer(_viewer.GetRenderer());
+            _window.RenderWindow.GetInteractor().SetInteractorStyle(imageStyle);
+            _viewer.Render();
+        }
+
+        public vtkRenderer GetRenderer()
+        {
+            return _viewer.GetRenderer(); ;
+        }
+
+   
+
+        /// <summary>
+        /// Changing the window width and level of the current slice.
+        /// </summary>
+        /// <param name="windowLevel"> Level of the window.</param>
+        /// <param name="windowWidth"> Width of the window.</param>
+        public void Update2DVisualization(float windowLevel, float windowWidth)
+        {
+            _windowLevel = windowLevel;
+            _windowWidth = windowWidth;
+          
+            UpdateViewer();
+
+        }
+
+        /// <summary>
+        /// Dispose class objects. 
+        /// </summary>
+        public bool Dispose()
+        {
+            if (_window != null) _window.Dispose();
+            if (_viewer != null) _viewer.Dispose();
+            return true;
+        }
+    }
+
+}
