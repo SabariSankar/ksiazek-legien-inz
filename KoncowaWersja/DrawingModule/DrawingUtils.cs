7bb2db584f81c92757420c7980fdaa38602dbe0f
glegien <glegien@gmail.com>
2012-12-10 22:43:54 +0000

    Działające rysowanie bez toolboxa

---
+++ KoncowaWersja/DrawingModule/DrawingUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kitware.VTK;
+
+namespace DrawingModule
+{
+    public class DrawingUtils
+    {
+        private int _lineWidth = 1;
+        public int LineWidth { get; set; }
+
+        private int _lineColor = 1;
+        public int LineColor { get; set; }
+
+        public static void Draw(vtkImageCanvasSource2D imageCanvas, DrawingInfo drawingInfo)
+        {
+            imageCanvas.SetDrawColor(234,123,123,234);
+            for (int j = 0; j < drawingInfo.Lines.Count; j++)
+            {
+                var line = drawingInfo.Lines[j];
+                if (line.PointList.Count > 1)
+                {
+                    DrawingPoint prev = line.PointList[0];
+                    for (int i = 1; i < line.PointList.Count; i++)
+                    {
+                        imageCanvas.FillTube(prev.X, prev.Y, line.PointList[i].X, line.PointList[i].Y, 2);
+                        prev = line.PointList[i];
+                    }
+                }
+            }
+        }
+
+    }
+}
