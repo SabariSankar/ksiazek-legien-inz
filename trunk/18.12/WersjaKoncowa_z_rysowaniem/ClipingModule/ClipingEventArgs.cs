bcb0bd4914fd62a68201e16fb666b4ae02d44581
glegien@gmail.com <glegien@gmail.com@de43d424-067d-3954-792a-a5482421c2c8>
2013-01-08 15:14:15 +0000

    Osobna klasa na arhument dla operacji przycięcia

---
+++ trunk/18.12/WersjaKoncowa_z_rysowaniem/ClipingModule/ClipingEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClipingModule
+{
+    /// <summary>
+    /// Enumeration with cliping operation type.
+    /// </summary>
+    public enum EClipingModuleOperationType
+    {
+        X1, X2, Y1, Y2, Z1, Z2
+    }
+
+    /// <summary>
+    /// Arguments of event used during cliping operation.
+    /// </summary>
+    public class ClipingEventArgs : EventArgs
+    {
+        public EClipingModuleOperationType Type { get; set; }
+        public int Position { get; set; }
+    }
+}
